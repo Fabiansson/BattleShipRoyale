43ae72b3d49f443f862613f842ba1af76d80c0f4
Fabiansson <fabianzbinden@outlook.com>
2020-06-13 13:27:17 +0200

    redis url

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,21 +1,23 @@
 import bluebird from 'bluebird';
 import redis from 'redis';
 import connectRedis from 'connect-redis';
-import { REDIS_HOST, REDIS_PORT, REDIS_PASS, REDIS_TTL } from '../helpers/constants';
+import { REDIS_HOST, REDIS_PORT, REDIS_PASS, REDIS_TTL, REDIS_URL } from '../helpers/constants';
 
 bluebird.promisifyAll(redis);
 
-const client = redis.createClient({
-  host: REDIS_HOST,
+const client = redis.createClient(REDIS_URL/*{
+  host: '192.168.45.4',
   port: REDIS_PORT,
+  url: REDIS_URL
   //password: REDIS_PASS
-});
+}*/);
 
 const redisStore = (expressSession) => {
   const redisStore = connectRedis(expressSession);
   const storeOptions = {
-    host: REDIS_HOST,
-    port: REDIS_PORT,
+    //host: REDIS_HOST,
+    //port: REDIS_PORT,
+    url: REDIS_URL,
     client: client,
     ttl: REDIS_TTL
   }
