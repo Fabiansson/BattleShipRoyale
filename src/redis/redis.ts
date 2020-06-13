0e246ff402eb158afb4ee85c91948e119821d2f6
Fabiansson <fabianzbinden@outlook.com>
2020-06-13 20:05:00 +0200

    added dotenv

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -5,19 +5,19 @@ import { REDIS_HOST, REDIS_PORT, REDIS_PASS, REDIS_TTL, REDIS_URL } from '../hel
 
 bluebird.promisifyAll(redis);
 
-const client = redis.createClient(REDIS_URL/*{
-  host: '192.168.45.4',
+const client = redis.createClient(/*REDIS_URL*/{
+  host: REDIS_HOST,
   port: REDIS_PORT,
-  url: REDIS_URL
+  //url: REDIS_URL
   //password: REDIS_PASS
-}*/);
+});
 
 const redisStore = (expressSession) => {
   const redisStore = connectRedis(expressSession);
   const storeOptions = {
-    //host: REDIS_HOST,
-    //port: REDIS_PORT,
-    url: REDIS_URL,
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    //url: REDIS_URL,
     client: client,
     ttl: REDIS_TTL
   }
