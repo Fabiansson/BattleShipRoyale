5601310724ac4dd0d63c8b27376256183c7babe5
Fabiansson <fabianzbinden@outlook.com>
2020-06-13 13:36:53 +0200

    typo

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -2,7 +2,7 @@ export const PORT = +process.env.PORT || 4000;
 export const HOST = process.env.HOST || '0.0.0.0';
 
 export const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
-export const REDIS_URL = process.env.REDS_URL || 'redis://127.0.0.1:6379/0';
+export const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379/0';
 export const REDIS_PORT = +process.env.REDIS_PORT || 6379;
 export const REDIS_PASS = process.env.REDIS_PASS || 'password';
 export const REDIS_TTL = 1000;
