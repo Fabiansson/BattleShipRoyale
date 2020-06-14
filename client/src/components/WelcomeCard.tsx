293b7c15e817b4fe778d0ccb96d64a832895b309
Fabiansson <fabianzbinden@outlook.com>
2020-06-15 00:08:20 +0200

    button css

--- client/src/components/WelcomeCard.tsx
+++ client/src/components/WelcomeCard.tsx
@@ -25,15 +25,15 @@ const useStyles = makeStyles({
   },
   title: {
     marginTop: "70px",
-    fontSize:20,
+    fontSize: 20,
     fontWeight: "bold"
   },
   shipstyle: {
-    height:"100px"
+    height: "100px"
   },
   logostyle: {
     marginTop: "70px",
-    height:"150px"
+    height: "150px"
   },
   button: {
     backgroundColor: "#56BAE8"
@@ -50,46 +50,46 @@ function WelcomeCard(props: any) {
   }
 
   const hostGame = () => {
-    props.socket.emit('open');   
+    props.socket.emit('open');
   }
 
-  const translate = (lang: string) =>{
+  const translate = (lang: string) => {
     i18n.changeLanguage(lang);
   }
 
   return (
     <div>
       <div className="languageButton">
-      <nav id="languageBar">
-    <Button  variant="contained" color="primary" onClick={() => translate('de')}>Deutsch</Button>
-    <Button  variant="contained" color="primary" onClick={() => translate('en')}>English</Button>
-    </nav>
-    </div>
-    <Paper elevation={0} className={classes.paper} >
- 
-      <Grid container spacing={3}>
-      <Grid item xs={12}>
-      <img src={logo} alt="our logo" className={classes.logostyle} />
-      </Grid>
-          <Grid item xs={12}>
-      <Button variant="contained" className={classes.button} onClick={joinGame}>{t('Join')}</Button>
-      </Grid>
-      <Grid item xs={12}>
-      <Button id="join" variant="contained" color="primary" onClick={hostGame}>{t('Host')}</Button>
-      </Grid>
-      <Grid item xs={12}>
-      <img src={ship} alt="our ship" className={classes.shipstyle} />
-      </Grid>
-      </Grid>
+        <nav id="languageBar">
+          <Button variant="contained" color="primary" onClick={() => translate('de')}>Deutsch</Button>
+          <Button variant="contained" color="primary" onClick={() => translate('en')}>English</Button>
+        </nav>
+      </div>
+      <Paper elevation={0} className={classes.paper} >
 
-    </Paper>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <img src={logo} alt="our logo" className={classes.logostyle} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" className={classes.button} onClick={joinGame}>{t('Join')}</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button id="join" variant="contained" color="primary" onClick={hostGame}>{t('Host')}</Button>
+          </Grid>
+          <Grid item xs={12}>
+            <img src={ship} alt="our ship" className={classes.shipstyle} />
+          </Grid>
+        </Grid>
+
+      </Paper>
     </div>
   );
 }
 
 const WelcomeCardWithSocket = (props: any) => (
   <SocketContext.Consumer>
-      {(socket: any) => <WelcomeCard {...props} socket={socket} />}
+    {(socket: any) => <WelcomeCard {...props} socket={socket} />}
   </SocketContext.Consumer>
 )
 
